1b8d1ec53ce4b3a0e815549535c2fb53cab634f4
Erick E. Trevino <erick.trevino@cloudlite.io>
2024-02-15 10:57:04 -0600

    Setup logger and errorHandler middleware. Add 404 page not found.

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ const cors = require('cors');
 
 const routes = require('./routes/root');
 const corsOptions = require('./config/corsOptions');
+const { logger } = require('./middleware/logger');
+const errorHandler = require('./middleware/errorHandler');
+
+app.use(logger);
 
 app.use(cors(corsOptions));
 
@@ -20,6 +24,19 @@ app.use(cookieParser());
 app.use('/', express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
+
+app.all('*', (req, res) => {
+  res.status(404);
+  if (req.accepts('html')) {
+    res.sendFile(path.join(__dirname, 'views', '404.html'));
+  } else if (req.accepts('json')) {
+    res.json({ message: '404 Not Found' });
+  } else {
+    res.type(txt).send('404 Not Found');
+  }
+});
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`);
