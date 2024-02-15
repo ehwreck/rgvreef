5d34f3d5953ab0991932b17c9c4d142176091949
Erick E. Trevino <erick.trevino@cloudlite.io>
2024-02-15 10:55:22 -0600

    Setup app to receive and parse json, cors and cookie-parser.

---
+++ config/allowedOrigins.js
@@ -0,0 +1,7 @@
+const allowedOrigins = [
+  'http://localhost:3000',
+  'https://www.rgvreef.org',
+  'https://rgvreef.org',
+];
+
+module.exports = allowedOrigins;
--- server.js
+++ server.js
@@ -3,7 +3,18 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3500;
 const path = require('path');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+
 const routes = require('./routes/root');
+const corsOptions = require('./config/corsOptions');
+
+app.use(cors(corsOptions));
+
+// Receive and parse json data
+app.use(express.json());
+
+app.use(cookieParser());
 
 // Server static images
 app.use('/', express.static(path.join(__dirname, 'public')));
